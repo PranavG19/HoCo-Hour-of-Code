766206cadffd3a9bca6f2db9b441ad2ed97b8a7c
Pranav <p.krishnan219@gmail.com>
2021-06-07 19:22:09 -0400

    fixed logout menu

--- public/script.js
+++ public/script.js
@@ -81,24 +81,19 @@ $(".login").on("click", function (e) {
 	e.preventDefault();
 	if (firebase.auth().currentUser) {
 		if ($(this).hasClass("navbar-font")) {
-			if ($("#logout")[0]) {
-				$("#logout").fadeOut(300, () => {
-					$("#logout").remove();
-				});
-			} else {
-				let w = $(".username").width();
-				$(
-					`<div id='logout' class='logoutMenu' style='width: ${w}px;'>Logout?<div>`
-				)
-					.hide()
-					.appendTo($(".username").parent().parent().parent())
-					.fadeIn(300, () => {
-						$("#logout").on("click", () => {
-							logout();
-							location.reload();
-						});
+			let w = $(".username").width();
+			$(
+				`<div id='logout' class='logoutMenu' style='width: ${w}px;'>Logout?<div>`
+			)
+				.hide()
+				.appendTo($(".username").parent().parent().parent())
+				.fadeIn(300, () => {
+					$("#logout").on("click", () => {
+						$("#logout").remove();
+						logout();
+						location.reload();
 					});
-			}
+				});
 		} else {
 			console.log("bye");
 			logout();
